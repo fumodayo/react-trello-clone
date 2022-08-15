5ff22240e3fa237cd392d42205bcacb9585f1e78
thairyo <sonthai1310.works@gmail.com>
2022-08-15 10:25:55 +0700

    Design trello clone Front-end.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,146 @@
-import './App.css';
+import React from 'react'
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="trello-thairyo-master">
+      <nav className="navbar-app">App Bar</nav>
+      <nav className="board-app">Board Bar</nav>
+      <div className="board-content">
+        <div className="column">
+          <header>Test 1</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/highway-landscape-colorful-sunset-road-260nw-2172624527.jpg"
+                alt="SEO"
+              />
+              title: tharyo test
+            </li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+          </ul>
+          <footer>Add new Item</footer>
+        </div>
+        <div className="column">
+          <header>Test 1</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/highway-landscape-colorful-sunset-road-260nw-2172624527.jpg"
+                alt="SEO"
+              />
+              title: tharyo test
+            </li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+          </ul>
+          <footer>Add new Item</footer>
+        </div>
+        <div className="column">
+          <header>Test 1</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/highway-landscape-colorful-sunset-road-260nw-2172624527.jpg"
+                alt="SEO"
+              />
+              title: tharyo test
+            </li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+          </ul>
+          <footer>Add new Item</footer>
+        </div>
+        <div className="column">
+          <header>Test 1</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/highway-landscape-colorful-sunset-road-260nw-2172624527.jpg"
+                alt="SEO"
+              />
+              title: tharyo test
+            </li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+          </ul>
+          <footer>Add new Item</footer>
+        </div>
+        <div className="column">
+          <header>Test 1</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/highway-landscape-colorful-sunset-road-260nw-2172624527.jpg"
+                alt="SEO"
+              />
+              title: tharyo test
+            </li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+          </ul>
+          <footer>Add new Item</footer>
+        </div>
+        <div className="column">
+          <header>Test 1</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/highway-landscape-colorful-sunset-road-260nw-2172624527.jpg"
+                alt="SEO"
+              />
+              title: tharyo test
+            </li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+          </ul>
+          <footer>Add new Item</footer>
+        </div>
+        <div className="column">
+          <header>Test 1</header>
+          <ul>
+            <li>
+              <img
+                src="https://image.shutterstock.com/image-photo/highway-landscape-colorful-sunset-road-260nw-2172624527.jpg"
+                alt="SEO"
+              />
+              title: tharyo test
+            </li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+            <li>Test li and test column aa askda</li>
+          </ul>
+          <footer>Add new Item</footer>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-const root = document.getElementById("root");
+const root = document.getElementById('root')
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   root
-);
+)
