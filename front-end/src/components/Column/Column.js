c94a63eae6a1c4e38c6293d122d56248da56c7f3
thairyo <sonthai1310.works@gmail.com>
2022-08-16 14:07:13 +0700

    Fix dropdown add new card.

--- front-end/src/components/Column/Column.js
+++ front-end/src/components/Column/Column.js
@@ -122,7 +122,9 @@ const Column = props => {
             />
 
             <Dropdown.Menu>
-              <Dropdown.Item>Add card...</Dropdown.Item>
+              <Dropdown.Item onClick={toggleOpenNewCardForm}>
+                Add card...
+              </Dropdown.Item>
               <Dropdown.Item onClick={toggleShowConfirmModal}>
                 Remove column...
               </Dropdown.Item>
